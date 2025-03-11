0ce8df730b980110689eb1fb2e64d00ce1e074e1
Clarete-ana <anaclaraclarete2@gmail.com>
2025-03-10 23:15:21 -0300

    Criado arquivo javascript com lógica inicial

---
+++ app.js
@@ -0,0 +1,23 @@
+function sortear(){
+  let quantidade = document.getElementById('quantidade').value;
+  let doNumero = parseInt(document.getElementById('de').value);
+  let ateNumero = parseInt(document.getElementById('ate').value);
+  
+  let sorteados = [];
+  let numero;
+
+  for(i = 0 ; i < quantidade ; i++){
+    numero = obterNumeroAleatorio(doNumero,ateNumero);
+    sorteados.push(numero);
+  }
+
+  alert(sorteados);
+
+  
+}
+
+function obterNumeroAleatorio(min,max){
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
